3729d547b4066cb065565a4e72227a1967de5fba
meltamps <meltamps@yahoo.com>
2015-06-26 18:24:24 -0700

    Update and rename Connection to Connection.java

--- Connection.java
+++ Connection.java
@@ -1,9 +1,3 @@
-@@ -0,0 +1,33 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package mbt.com;
 
 import java.sql.Connection;
